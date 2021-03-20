5622c58176e6dbb14563c6ff221d69ac1671985f
Артём Келиогло <76010430+Artem-Kelioglo@users.noreply.github.com>
2021-03-20 15:12:15 +0200

    17 дз убрал функцию лишнюю

--- ДЗ2!/homework 17/js.js
+++ ДЗ2!/homework 17/js.js
@@ -2,42 +2,30 @@ let in1 = document.querySelector('#input1')
 let in2 = document.querySelector("#input2")
 let red = document.querySelector('#redblock')
 let green = document.querySelector('#greenblock')
-// let block = document.querySelector(".block")
+
 in1.onchange = function () {
   in2.value = in1.value
-  good()
-  // green.style.height = in1.value + 'px'
-  // red.style.height = cul(in1) + 'px'
-  // red.style.bottom = green.style.height
+  good(in2)
 }
 in2.onchange = function () {
   in1.value = in2.value
-  // green.style.height = in1.value + 'px'
-  // red.style.height = cul(in1) + 'px'
-  // red.style.bottom = green.style.height
-  good()
+  good(in1)
 }
 
-function good() {
-  green.style.height = in1.value + 'px'
-  red.style.height = cul(in1) + 'px'
+function good(i) {
+  green.style.height = i.value + 'px'
   red.style.bottom = green.style.height
-
-  function cul(arg) {
-    let res = 0
-    if (arg.value < 20 && arg.value >= 0) {
-      res = arg.value * 0.02
-    }
-    if (arg.value >= 20 && arg.value < 50) {
-      res = arg.value * 0.04
-    }
-    if (arg.value >= 50 && arg.value < 75) {
-      res = arg.value * 0.06
-    }
-    if (arg.value >= 75 && arg.value <= 110) {
-      res = arg.value * 0.08
-    }
-
-    return res
+  if (i.value < 20 && i.value >= 0) {
+   return red.style.height = i.value * 0.02 + 'px'
   }
+  if (i.value >= 20 && i.value < 50) {
+   return red.style.height = i.value * 0.04 + 'px'
+  }
+  if (i.value >= 50 && i.value < 75) {
+   return red.style.height = i.value * 0.06 + 'px'
+  }
+  if (in1.value >= 75 && i.value <= 110) {
+   return red.style.height = i.value * 0.08 + 'px'
+  }
+
 }
