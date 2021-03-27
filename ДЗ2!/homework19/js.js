4284089550c566a7c93227e49c6fc00b9a7da4dd
Артём Келиогло <76010430+Artem-Kelioglo@users.noreply.github.com>
2021-03-27 19:48:42 +0200

    дз19(2)

--- ДЗ2!/homework19/js.js
+++ ДЗ2!/homework19/js.js
@@ -1,26 +1,24 @@
-table = document.querySelector('table')
+table = document.querySelector('table');
 
 table.addEventListener('click', function () {
-  let yach = event.target
-
-  let zn = yach.innerText
+  let yach = event.target;
+  let zn = yach.innerText;
   if (yach.tagName == "TD") {
-    yach.innerHTML = ''
-    let textar = document.createElement('textarea')
-    textar.value = zn
-    let button = document.createElement('button')
-    button.innerHTML = 'отправить'
-    let cales = document.createElement('button')
-    cales.innerHTML = 'отмена'
-    yach.append(textar)
-    yach.append(button)
-    yach.append(cales)
+    yach.innerHTML = '';
+    let textar = document.createElement('textarea');
+    textar.value = zn;
+    let button = document.createElement('button');
+    button.innerHTML = 'сохранить';
+    let cales = document.createElement('button');
+    cales.innerHTML = 'отмена';
+    yach.append(textar);
+    yach.append(button);
+    yach.append(cales);
     button.onclick = function () {
-      console.log('dsf')
-      yach.innerHTML = textar.value
+      yach.innerHTML = textar.value;
     }
     cales.onclick = function () {
-      yach.innerHTML = zn
+      yach.innerHTML = zn;
     }
   }
-})
+});
