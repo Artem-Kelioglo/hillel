16c093063d71acc0ffcacf64a12f45207b0bd741
Артём Келиогло <76010430+Artem-Kelioglo@users.noreply.github.com>
2021-03-30 21:28:59 +0300

    final

--- ДЗ2!/homework 17/js.js
+++ ДЗ2!/homework 17/js.js
@@ -3,29 +3,36 @@ let in2 = document.querySelector("#input2")
 let red = document.querySelector('#redblock')
 let green = document.querySelector('#greenblock')
 
-in1.oninput  = function () {
+in1.addEventListener('input', function () {
   in2.value = in1.value
-  good(in2)
-}
-in2.oninput  = function () {
+  good()
+})
+
+
+in2.addEventListener('input', function () {
   in1.value = in2.value
-  good(in1)
+  good()
+})
+
+function good() {
+  green.style.height = in1.value + 'px'
+  red.style.bottom = green.style.height
+
+  if (in1.value < 20 && in1.value >= 0) {
+    return calcCommission(0.02)
+  }
+  if (in1.value >= 20 && in1.value < 50) {
+    return calcCommission(0.04)
+  }
+  if (in1.value >= 50 && in1.value < 75) {
+    return calcCommission(0.06)
+  }
+  if (in1.value >= 75 && in1.value <= 110) {
+    return calcCommission(0.08)
+  }
+
 }
 
-function good(i) {
-  green.style.height = i.value + 'px'
-  red.style.bottom = green.style.height
-  if (i.value < 20 && i.value >= 0) {
-   return red.style.height = i.value * 0.02 + 'px'
-  }
-  if (i.value >= 20 && i.value < 50) {
-   return red.style.height = i.value * 0.04 + 'px'
-  }
-  if (i.value >= 50 && i.value < 75) {
-   return red.style.height = i.value * 0.06 + 'px'
-  }
-  if (in1.value >= 75 && i.value <= 110) {
-   return red.style.height = i.value * 0.08 + 'px'
-  }
-
+function calcCommission(prosent) {
+  red.style.height = in1.value * prosent + 'px'
 }
