6dacf78e551df2c4c3f2e77a21ec743cbd5711a1
Artem-Kelioglo <76010430+Artem-Kelioglo@users.noreply.github.com>
2021-04-08 19:44:32 +0300

    пр

--- ДЗ2!/homework19/js.js
+++ ДЗ2!/homework19/js.js
@@ -23,55 +23,6 @@
 //   }
 // });
 
-// const table = document.querySelector('#table');
-// const actions = {
-//   save(target) {
-//     let textArea = target.querySelector('textarea');
-//     target.innerHTML = textArea.value;
-//   },
-//   cancel(target) {
-//     let textArea = target.querySelector('textarea');
-//     target.innerHTML = textArea.dataset.oldval;
-//   }
-// }
-// table.addEventListener('click', createInput);
-
-// function createInput() {
-//   const target = event.target;
-//   if (target.tagName != 'TD' || (target.tagName == 'TD' && target.children.length != 0)) return;
-//   let fragment = createFragment(target);
-//   target.innerHTML = null;
-//   target.append(fragment);
-// }
-
-// function click(target) {
-//   const action = this.dataset.action;
-//   actions[action] && actions[action](target);
-// }
-
-// function createFragment(target) {
-//   let fragment = document.createDocumentFragment();
-//   let textArea = createTextArea(target.innerHTML);
-//   var btns = Object.keys(actions).map(key => createBtn(key, target));
-//   fragment.append(textArea);
-//   btns.forEach(btn => fragment.append(btn));
-//   return fragment;
-// }
-
-// function createTextArea(value) {
-//   let textArea =document.createElement('textarea');
-//     textArea.setAttribute(`data-oldval`, value);
-//   textArea.value = value;
-//   return textArea;
-// }
-
-// function createBtn(action, target) {
-//   let btn = document.createElement('button');
-//   btn.setAttribute(`data-action`, action);
-//   btn.innerHTML = action;
-//   btn.onclick = click.bind(btn, target);
-//   return btn;
-// }
 
 
 let table = document.querySelector('#table');
